112cc9562fdb6d08fd49b7e6a76d885deab26f77
Moez Khalid <29011087+mightymozdef@users.noreply.github.com>
2024-04-30 00:23:07 -0400

    Added playlist information to playlist tracks component and changed styles - still WIP

--- frontend/spotify-app/src/app/components/playlist-tracks/playlist-tracks.component.ts
+++ frontend/spotify-app/src/app/components/playlist-tracks/playlist-tracks.component.ts
@@ -11,8 +11,12 @@ import { SpotifyService } from 'src/app/services/spotify-service.service';
 })
 export class PlaylistTracksComponent {
   playlistTracks: any[] = [];
+  playlistTrackDurations: number[] = [];
   playlistInformation: any[] = [];
   playlistImageURL: string = '';
+  playlistDescription: string = '';
+  playlistTracksTotal: number = 0;
+  playlistTotalTime: number = 0;
 
   constructor(
     private spotifyService: SpotifyService,
@@ -21,18 +25,39 @@ export class PlaylistTracksComponent {
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id') ?? '';
-    console.log(id);
+    // console.log(id);
 
     if (id !== '') {
       this.spotifyService.getPlaylist(id).subscribe((p) => {
-        console.log(p);
+        // console.log(p);
+        this.playlistDescription = p.description;
         this.playlistInformation.push(p);
         this.playlistImageURL = this.playlistInformation[0].images[0].url;
+        this.playlistTracksTotal = p.tracks.total;
       });
       this.spotifyService.getPlaylistTracks(id).subscribe((p) => {
+        // console.log(p);
         this.playlistTracks.push(p);
-        console.log(this.playlistTracks);
+
+        for (let song of p) {
+          console.log(song);
+          this.playlistTrackDurations.push(song.track.durationMs);
+          this.playlistTotalTime += song.track.durationMs;
+        }
+        console.log(this.playlistTotalTime);
       });
     }
   }
+
+  //needed to convert the total playlist duration (each track added up) to be displayed correctly
+  convertMsToTime = (ms: number): string => {
+    const totalSeconds = ms / 1000; //convert ms to s
+    const hours = Math.floor(totalSeconds / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = Math.floor((totalSeconds % 3600) / 3600);
+    const formattedHrs = hours < 10 ? '0' + hours : hours.toString();
+    const formattedMins = minutes < 10 ? '0' + minutes : minutes.toString();
+    const formattedSecs = seconds < 10 ? '0' + seconds : seconds.toString();
+    return `${formattedHrs} hr ${formattedMins} min ${formattedSecs} sec`;
+  };
 }
